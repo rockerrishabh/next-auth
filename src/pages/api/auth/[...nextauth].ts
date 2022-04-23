68bee7dc72ace03860a798c21726b31a9590797e
rockerrishabh <rishabh.6542@gmail.com>
2022-04-23 18:03:08 +0530

    third-commit

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,56 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import { prisma } from "../../../../lib/prisma";
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  session: {
+    // Choose how you want to save the user session.
+    // The default is `"jwt"`, an encrypted JWT (JWE) in the session cookie.
+    // If you use an `adapter` however, we default it to `"database"` instead.
+    // You can still force a JWT session by explicitly defining `"jwt"`.
+    // When using `"database"`, the session cookie will only contain a `sessionToken` value,
+    // which is used to look up the session in the database.
+    strategy: "database",
+  },
+  callbacks: {
+    redirect: async (params: { url: string; baseUrl: string }) =>
+      Promise.resolve(params.url),
+    session: async ({ session, user }: { session: any; user: any }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+          role: user.role,
+        },
+      };
+    },
+  },
 });
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      image?: string | null;
+      role?: string;
+    };
+  }
+}
